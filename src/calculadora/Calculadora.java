fd75ac2226b083998441263bfa458b65934942d7
XXX <>
2019-08-29 19:36:32 -0500

    Adiciona clase para leer de consola

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sala 1
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Holiiiiiiiiiiiiiiii");
+        Scanner sc=new Scanner(System.in);
+        String dato=sc.nextLine();
+        
+    }
+    
+}
